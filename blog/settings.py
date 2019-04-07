6a48280c1b6d97bdf7d9e26ee08f38a7b2855ad1
Anuj Singh <44033673+anuj920@users.noreply.github.com>
2019-04-07 23:29:10 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -48,7 +48,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mytechblog.anujblog@gmail.com'
-EMAIL_HOST_PASSWORD = 'mytechblog'
+EMAIL_HOST_PASSWORD = '********'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
